09c58dd5b3a43c2ddefbbb819e0a363fba7515c2
DeveloperRaj <rajchhatrala2001@gmail.com>
2019-11-29 16:29:26 +0530

    initial commit

---
+++ public/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SPrime Services</title>
+	<link rel="stylesheet" type="text/css" href="public/styles/header.css">
+</head>
+<body>
+
+	<header>
+		<section class="logo"><span style="margin-right: 10px;font-size: 40px"><b style="color: red;background-color: rgb(242,242,240);">E</b>PIC</span> <b style="color: red;background-color: rgb(242,242,240);">S</b>ervices</section>
+		<section class="navigation">
+			<a href="#">Home</a>
+			<a href="#">Personal Notes</a>
+			<a href="#">Stories</a>
+			<a href="#">News Flash</a>
+			<a href="#" style="color: red; background-color: rgb(242,242,240);padding: 10px 10px 10px 10px;font-weight: bolder;">Login</a>
+		</section>
+	</header>
+
+</body>
+</html>
